734b34fe6ced976ff7f0c7a0584faef50b2a05ab
Dmitriy Ivliev <rootkit89@gmail.com>
2016-04-02 14:11:42 +0400

    fix solution and remove comments

--- exercise/solution.js
+++ exercise/solution.js
@@ -1,34 +1,7 @@
 import MORSE_CODE from './helper';
 
-// BEGIN (write your solution here)
+// BEGIN
 let morseDecode = (code) => {
-  // let splitWord = (word) => {
-  //   return word.split(' ');
-  // };
-
-  // let splitString = (str) => {
-  //   return str.split('   ');
-  // }
-
-  // code = code.trim();
-
-  // if (code.length == 0){
-  //   return '';
-  // }
-
-  // let result = '';
-
-  // let wordsArr = splitString(code);
-  // for(let word in wordsArr){
-  //   let letArr = splitWord(wordsArr[word]);
-  //   for(let letter in letArr) {
-  //     result += MORSE_CODE[letArr[letter]];
-  //   };
-
-  //   result += ' ';
-  // }
-  // return result.slice(0, -1);
-
   let decodeLetter = (code) => {
     return MORSE_CODE[code];
   };
